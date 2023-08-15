b5426eca14f31f62b72a6b0a95c53f12ed032b4b
AroojMZahid <aroojzahid.ideegames@gmail.com>
2023-08-15 17:27:57 +0500

    add seperate file for data

--- components/Expertise/Expertise.tsx
+++ components/Expertise/Expertise.tsx
@@ -1,27 +1,8 @@
-import { Box, Flex, Grid, Heading } from '@chakra-ui/react'
+import { Flex, Grid, Heading } from '@chakra-ui/react'
 
 import { ExpertiseCard } from './ExpertiseCard'
-
-const expertiseData = [
-  {
-    title: 'Web',
-    boxLineWidth: '25px',
-    iconIndex: 0,
-    desc: 'We are a leading company at the forefront of the digital landscape, offering a diverse range of cutting-edge products in the domains of AI, Blockchain, Gaming, and Web2.0. Our expertise lies in delivering exceptional graphic design, website development, and top-notch web solutions. With a focus on optimizing Google page rankings and ensuring robust website security, we are committed to providing unparalleled digital marketing services. Through our innovative approach and a team of experts,we empower businesses to thrive in the dynamic and competitive online ecosystem.',
-  },
-  {
-    title: 'Video Games',
-    boxLineWidth: '100px',
-    iconIndex: 1,
-    desc: 'Our team creates captivating games with immersive gameplay, stunning visuals, and engaging storylines. From action-packed adventures to mind-bending puzzles, our diverse range of titles offers something for every player. Get ready for thrilling quests and endless excitement in the world of Maxovation.',
-  },
-  {
-    title: 'Services',
-    boxLineWidth: '60px',
-    iconIndex: 2,
-    desc: 'Maxovation excels at transforming innovative concepts into tangible solutions. From web and mobile applications to software and digital products, we bring your ideas to life efficiently and effectively. Partner with us to make your vision a reality.',
-  },
-]
+import { ExpertiseData } from './Expertisedata'
+import { map } from 'lodash'
 
 const Expertise = () => {
   return (
@@ -47,24 +28,16 @@ const Expertise = () => {
         marginY={10}
         gap={20}
       >
-        <ExpertiseCard
-          title={expertiseData[0].title}
-          desc={expertiseData[0].desc}
-          boxLineWidth={expertiseData[0].boxLineWidth}
-          iconIndex={expertiseData[0].iconIndex}
-        />
-        <ExpertiseCard
-          title={expertiseData[1].title}
-          desc={expertiseData[1].desc}
-          boxLineWidth={expertiseData[1].boxLineWidth}
-          iconIndex={expertiseData[1].iconIndex}
-        />
-        <ExpertiseCard
-          title={expertiseData[2].title}
-          desc={expertiseData[2].desc}
-          boxLineWidth={expertiseData[2].boxLineWidth}
-          iconIndex={expertiseData[2].iconIndex}
-        />
+        {map(ExpertiseData, (data) => {
+          return (
+            <ExpertiseCard
+              title={data.title}
+              desc={data.desc}
+              boxLineWidth={data.boxLineWidth}
+              iconIndex={data.iconIndex}
+            />
+          )
+        })}
       </Grid>
     </Flex>
   )
