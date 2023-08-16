f2a1707e0aadb2ab3cb1575967188be37ff6453e
Shahvaiz Jahangeer <38224697+shahvaizj@users.noreply.github.com>
2023-08-16 19:46:47 +0500

    In process. Components made but need review by arooj

--- pages/Home/Home.tsx
+++ pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import { PrimaryLayout } from '@components/Layout'
 import { Banner } from '@components/Banner'
+import { CompanyStats } from '@components/CompanyStats'
 import React from 'react'
 
 const Home = () => {
   return (
     <PrimaryLayout>
       <Banner />
+      <CompanyStats />
     </PrimaryLayout>
   )
 }
