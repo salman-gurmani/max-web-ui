b8986b704658ec86e768c8908c6cbbcbb07c47d5
AroojMZahid <aroojzahid.ideegames@gmail.com>
2023-08-23 14:41:32 +0500

    mobile responsive add

--- components/CompanyStats/CompanyStats.tsx
+++ components/CompanyStats/CompanyStats.tsx
@@ -1,20 +1,22 @@
 import { Flex, Grid } from '@chakra-ui/react'
 
-import {StatsData} from "./StatsData"
-import StatsView from "./StatsView"
+import { StatsData } from './StatsData'
+import StatsView from './StatsView'
 
 import { map } from 'lodash'
 
 const CompanyStats = () => {
   return (
     <Flex direction="column" bg="#18191D">
-
       <Grid
-        templateColumns={{ base: '1fr 1fr', md: '1fr 1fr', lg: '1fr 1fr 1fr 1fr' }}
-        marginLeft={100}
-        marginRight={100}
+        templateColumns={{
+          base: '1fr 1fr',
+          md: '1fr 1fr',
+          lg: '1fr 1fr 1fr 1fr',
+        }}
+        marginX={{ base: 10, md: 50, lg: 100 }} // margin left and right have same value so insteaf og marginLeft and marginRight use marginX
         marginY={30}
-        gap={10}
+        gap={{ base: 2, md: 10, lg: 20 }} // gap for different screen sizes
       >
         {map(StatsData, (data) => {
           return (
