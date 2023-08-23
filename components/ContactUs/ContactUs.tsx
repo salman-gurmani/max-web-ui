5e9066490841331ef5368880928bfbdecb892af3
AroojMZahid <aroojzahid.ideegames@gmail.com>
2023-08-23 18:56:39 +0500

    contact us form and map add

--- components/ContactUs/ContactUs.tsx
+++ components/ContactUs/ContactUs.tsx
@@ -8,12 +8,12 @@ import {
   GridItem,
   Input,
   AspectRatio,
+  Textarea,
 } from '@chakra-ui/react'
 import { useFormik } from 'formik'
+import React from 'react'
 
 const ContactUs = () => {
-  const columns = { base: 1, md: 2, lg: 2 }
-
   const formik = useFormik({
     initialValues: {
       name: '',
@@ -22,9 +22,11 @@ const ContactUs = () => {
       subject: '',
       message: '',
     },
-    onSubmit: async () => {},
+    onSubmit: () => {},
   })
-  const { handleSubmit } = formik
+
+  const { handleChange, handleSubmit, values } = formik
+
   return (
     <Flex bg="#18191D" direction="column">
       <Flex alignContent="center" justifyContent="center">
@@ -44,72 +46,103 @@ const ContactUs = () => {
       <>
         <form onSubmit={handleSubmit}>
           <Grid
-            templateColumns={{ base: '1fr', md: '50% 50%', lg: '70% 30%' }}
+            templateColumns={{ base: '1fr', md: '50% 50%', lg: '65% 35%' }}
             marginY={10}
             marginX={{ base: '5', md: '10', lg: '20' }}
             gap={{ base: 10, md: 10, lg: 1 }}
           >
-            <Box bg="#0A0C19" padding={7} borderRadius="14px 0px 0px 14px">
-              <Grid templateColumns={`repeat(${columns}, 1fr)`} gap={2} p={4}>
-                <GridItem>
+            <Box bg="#0A0C19" padding={12} borderRadius="14px 0px 0px 14px">
+              <Grid
+                templateColumns={{ base: '1fr', md: '1fr 1fr', lg: '1fr 1fr' }}
+                gap={5}
+              >
+                <GridItem mb={5}>
                   <FormControl id="name" isRequired>
                     <Input
                       placeholder="Enter your name"
-                      textColor="white"
                       backgroundColor="#171926"
                       type="text"
                       name="name"
                       as={Input}
+                      height={50}
+                      border="none"
+                      color="white"
+                      _placeholder={{ color: '#CFCFCF' }}
+                      fontFamily="roboto"
+                      size="sm"
                     />
                   </FormControl>
                 </GridItem>
-                <GridItem>
+                <GridItem mb={5}>
                   <FormControl id="phoneNumber" isRequired>
                     <Input
-                      placeholder="Enter your phone number"
-                      textColor="white"
+                      placeholder="Enter your phone"
                       backgroundColor="#171926"
                       type="text"
                       name="phoneNumber"
                       as={Input}
+                      height={50}
+                      border="none"
+                      color="white"
+                      _placeholder={{ color: '#CFCFCF' }}
+                      fontFamily="roboto"
+                      size="sm"
                     />
                   </FormControl>
                 </GridItem>
-                <GridItem>
-                  <FormControl pt={3} id="email" isRequired>
+                <GridItem mb={5}>
+                  <FormControl id="email" isRequired>
                     <Input
                       placeholder="Enter your Email"
-                      textColor="white"
                       backgroundColor="#171926"
                       type="email"
+                      height={50}
+                      border="none"
+                      color="white"
+                      _placeholder={{ color: '#CFCFCF' }}
+                      fontFamily="roboto"
+                      size="sm"
                     />
                   </FormControl>
                 </GridItem>
 
-                <GridItem>
+                <GridItem mb={5}>
                   <FormControl id="subject" isRequired>
                     <Input
                       placeholder="Enter your subject"
-                      textColor="white"
                       backgroundColor="#171926"
                       type="text"
                       name="subject"
                       as={Input}
+                      height={50}
+                      border="none"
+                      color="white"
+                      _placeholder={{ color: '#CFCFCF' }}
+                      fontFamily="roboto"
+                      size="sm"
                     />
                   </FormControl>
                 </GridItem>
-                <GridItem>
-                  <FormControl id="message" isRequired>
-                    <Input
-                      placeholder="Type your message here"
-                      textColor="white"
-                      backgroundColor="#171926"
-                      type="text"
-                      name="message"
-                      as={Input}
-                    />
-                  </FormControl>
-                </GridItem>
+              </Grid>
+              <Flex mt={5}>
+                <FormControl id="message" isRequired>
+                  <Textarea
+                    name="message"
+                    value={values.message}
+                    onChange={handleChange}
+                    bg="#171926"
+                    placeholder="Type your message here..."
+                    border="none"
+                    color="white"
+                    _placeholder={{ color: '#CFCFCF', verticalAlign: 'top' }}
+                    fontFamily="roboto"
+                    size="sm"
+                    minHeight={340}
+                    maxHeight={400}
+                  />
+                </FormControl>
+              </Flex>
+              <Flex justifyContent="center" mt={5}>
                 <Button
                   color="white"
                   fontFamily="roboto"
@@ -122,7 +155,7 @@ const ContactUs = () => {
                   background="var(--gradient, linear-gradient(135deg, #2FBBFB 0%, #D442E0 52.60%, #F15D3C 100%))"
                   paddingX={{ base: '3', md: '5', lg: '5' }}
                   zIndex="5"
-                  alignSelf="flex-start"
+                  alignSelf="center"
                   _hover={{
                     bgGradient:
                       'linear-gradient(95.1deg, #2FBBFB 0%, #D442E0 52.6%, #F15D3C 100%)',
@@ -134,7 +167,7 @@ const ContactUs = () => {
                 >
                   Send Message
                 </Button>
-              </Grid>
+              </Flex>
             </Box>
 
             <AspectRatio style={{ position: 'relative', background: 'black' }}>
