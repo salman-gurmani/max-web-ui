d5faf38384b4491a9c6f7dc1ea95ab7099f9e837
AroojMZahid <aroojzahid.ideegames@gmail.com>
2023-08-28 19:21:18 +0500

    svgs shifted to public folder

--- pages/Home/Home.tsx
+++ pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 import { PrimaryLayout } from '@components/Layout'
 import { Banner } from '@components/Banner'
-
+import { GameTechnologies } from '@components/GameTechnologies'
 import { WebTechnologies } from '@components/WebTechnologies'
 
 import React from 'react'
@@ -10,6 +10,7 @@ const Home = () => {
     <PrimaryLayout>
       <Banner />
       <OurGameProject />
+      <GameTechnologies />
       <WebTechnologies />
     </PrimaryLayout>
   )
