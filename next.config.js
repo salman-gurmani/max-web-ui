712f1c74291611756e510dfc06ccbca6eac7fde2
AroojMZahid <aroojzahid.ideegames@gmail.com>
2023-09-13 15:09:46 +0500

    image issue fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  images: {
+    domains: ['localhost', 'res.cloudinary.com'],
+    unoptimized: true,
+  },
   reactStrictMode: false,
   webpack(config, { isServer }) {
     const prefix = config.assetPrefix ?? config.basePath ?? ''
