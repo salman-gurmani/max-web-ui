ab0d6d99e8559add167e87c268885ba4694b56f4
AroojMZahid <aroojzahid.ideegames@gmail.com>
2023-09-15 15:07:37 +0500

    fix project details Image issue

--- pages/projects/[projectId].tsx
+++ pages/projects/[projectId].tsx
@@ -12,7 +12,7 @@ import {
 } from '@chakra-ui/react'
 import React from 'react'
 import { WebsiteDetailBanner } from '@components/WebisteDetailBanner'
-import { ProjectDetailsImages } from '@components/Images'
+
 import { TextOnImagePanel } from '@components/TextOnImagePanel'
 import { BoxList } from '@components/BoxList'
 import { BoxTable } from '@components/BoxTable'
@@ -21,6 +21,7 @@ import { find, map } from 'lodash'
 import { EllipseImage } from '@components/Images'
 import { Footer } from '@components/Footer'
 import { BackToHomeNavBar } from '@components/BackToHomeNavBar'
+import { CldImage } from 'next-cloudinary'
 
 const WebsiteDetail = () => {
   const router = useRouter()
@@ -62,13 +63,13 @@ const WebsiteDetail = () => {
           >
             <VStack alignItems="left" justifyContent="left" spacing={6}>
               <Box overflow="hidden" position="relative">
-                <Image
+                <CldImage
                   src={webData.image}
                   alt=""
-                  h="100%"
+                  width="1920"
+                  height={1080}
                   style={{
                     zIndex: 0,
-                    width: '100%',
                   }}
                 />
               </Box>
@@ -122,9 +123,11 @@ const WebsiteDetail = () => {
                     display="flex"
                     alignItems="center"
                   >
-                    <Image
-                      src={ProjectDetailsImages.checkmark.src.src}
+                    <CldImage
+                      src="v1694585526/Icons/ProjectDetails/checkmark_kfgivm.svg"
                       alt=""
+                      width={30}
+                      height={30}
                     />
                     <span style={{ marginLeft: '20px' }}>{data}</span>
                   </ListItem>
